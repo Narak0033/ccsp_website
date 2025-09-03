41030cc00bce2cfdb961a1e052b52c5aca42e1e2
dana <lydana32@gmail.com>
2025-09-03 10:28:49 +0700

    feat: implement project feature on project page

--- ccsp_website/app/[locale]/projects/page.tsx
+++ ccsp_website/app/[locale]/projects/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import ProjectCard from '@/components/Project/ProjectCard'
 const AllProjects = () => {
   return (
-    <div>
-      
+    <div className='overflow-hidden'>
+            <div className="container pt-20 my-20 ">
+        {/* Header Section */}
+        <div className="flex justify-center items-center flex-col space-y-6 mb-12">
+          <div className="text-center space-y-4">
+            <h1 className="text-header">
+              CCSP Projects
+            </h1>
+          </div>
+          
+          <p className="text-center text-gray-200 subtext ">
+            Highlighting the diverse projects organized by student clubs, dedicated to connecting knowledge with community service.
+          </p>
+        </div>
+          <ProjectCard />
+      </div>
     </div>
   )
 }
--- ccsp_website/components/Home/FAQ/QuestionAnswer.tsx
+++ ccsp_website/components/Home/FAQ/QuestionAnswer.tsx
@@ -63,7 +63,7 @@ const QuestionAnswer = () => {
 
       {/* Contact Section */}
       <aside
-        className="gap-6 my-12 max-w-6xl mx-auto"
+        className="gap-6 my-12 max-w-5xl mx-auto"
         aria-labelledby="faq-contact-title"
       >
         <Card
@@ -71,7 +71,7 @@ const QuestionAnswer = () => {
           role="region"
           aria-labelledby="faq-contact-title"
         >
-          <div className="relative z-10 max-w-4xl mx-auto">
+          <div className="relative z-10 max-w-3xl mx-auto">
             <h3
               id="faq-contact-title"
               className="text-xl md:text-2xl lg:text-3xl font-bold mb-6 leading-relaxed text-center"
